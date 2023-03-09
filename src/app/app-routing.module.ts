5cb4e245f09e1d83035070c61c21687398869d6c
cintia <cintiasilva7300@gmail.com>
2023-03-09 19:29:05 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './screens/login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+
+  {
+    path: '', component: HomeComponent
+  },
+
+  {
+    path: '**', pathMatch: 'full',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './screens/home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardComponent } from './components/carousel/card/card.component';
+import { LoginComponent } from './screens/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    HomeComponent,
+    CarouselComponent,
+    CardComponent,
+    LoginComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
