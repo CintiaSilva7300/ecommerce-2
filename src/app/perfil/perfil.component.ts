3160b063c08f31856f9259f499ed4b46f811cd79
cintia <cintiasilva7300@gmail.com>
2023-03-16 21:36:12 -0300

    created perfil user avatar

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -9,17 +9,20 @@ export class PerfilComponent implements OnInit {
   email!: any;
   name!: any;
   id: any
+  genre: any;
 
   constructor(){
-    this.id = ''
-    this.name = ''
-    this.email = ''
+    this.id = '';
+    this.name = '';
+    this.email = '';
+    this.genre = '';
   }
 
   ngOnInit(): void {
     this.name = localStorage.getItem('name');
     this.id = localStorage.getItem('id');
     this.email = localStorage.getItem('email');
+    this.genre = localStorage.getItem('genre');
     console.log('id: ',this.id,);
     console.log('email: ',this.email);
     console.log('name: ',this.name);
